8e3fb29a489a306a0408efbe0d45796fcffc094e
Jennie Kerfstedt <jenniekerf@gmail.com>
2019-01-07 17:13:18 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,28 +1,31 @@
 
-def line(katz_deli)
-  line_positions = []
-if katz_deli.empty?
-  puts "The line is currently empty."
-else
-  number = 1 
-  katz_deli.each do |customer|
-    line_positions << "#{number}. #{customer}"
-    number += 1
-end
-puts "The line is currently: #{line_positions.join(" ")}"
+def line(deli)
+  if deli.empty?
+    puts "The line is currently empty."
+  else
+    new_line = "The line is currently:"
+    deli.each.with_index(1) {|name, number|
+    new_line << " #{number}. #{name}"}
+    puts new_line
 end
 end
 
-def take_a_number(katz_deli, name)
-  katz_deli << name
-puts "Welcome, #{name}. You are number #{katz_deli.size} in line."
+def take_a_number(array, name)
+  array << name
+  puts "Welcome, #{name}. You are number #{array.size} in line."
 end
-
-def now_serving(katz_deli)
-  if katz_deli.empty?
+now_serving(["Jen", "Joe"])
+def now_serving(array)
+  if array.empty?
     puts "There is nobody waiting to be served!"
   else
-    puts "Currently serving #{katz_deli.first}."
-    katz_deli.shift
-  end
+    puts "Currently serving #{array.shift}."
+    
 end
+end
+
+
+
+
+
+ 
